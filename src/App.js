798c67bb49e625ef5f4fabba91a1e7c846200af3
Het Patel <137598780+Het2604@users.noreply.github.com>
2024-01-05 17:18:00 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Data from './Components/Data';
+import Header from './Components/Header';
+
+function App() {
+  return (
+    <>
+      <div>
+        <Header />
+        <Data />
+      </div>
+    </>
+  );
+}
+
+export default App;
